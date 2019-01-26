8438cd53b8110b92a4718d6d048974d9cc7d4cda
Belyaev Dmitry <belyaev@reg.ru>
2019-01-26 23:04:25 +0300

    remove comments

--- tester.go
+++ tester.go
@@ -37,37 +37,30 @@ func main() {
 
 	// ======================== главная / регистрация / логин / выход =====================
 
-	// ------------------ маршрут к главной странице -----------------
 	router.GET("/", app.ShowMainPage)
 
-	// ----------------- форма логина -----------------------------
 	router.GET("/login", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "login.html", c.Keys)
 	})
 
-	// ---------------- обработка данных формы логина -----------------
 	router.POST("/login/end", app.LoginEnd)
 
-	// --------------------- разлогинивание --------------------------
 	router.GET("/logout", func(c *gin.Context) {
 		// для разлогина сохраним емаил, по которому пользователь не найдется
 		app.SaveEmailToSession(c, "")
 		app.ShowMainPage(c)
 	})
 
-	// ----------------- форма регистрации на сайте ----------------------
 	router.GET("/registration/start", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "registration.html", c.Keys)
 	})
 
-	// ----------- обработка формы регистрации нового пользователя ------------
 	router.POST("/registration/end", app.RegistrationEnd)
 
 	// ================== площадки =========================================
 
 	locations := router.Group("/locations")
 	{
-		// ------------ список площадок -----------------------------------
 		locations.Any("/", app.ShowLocations)
 
 		// ниже этого будет требовать залогиниться:
@@ -75,16 +68,13 @@ func main() {
 		// требование быть админом:
 		locations.Use(app.GotoAccessDeniedIfNotAdmin)
 
-		// ------------- форма добавления новой площадки ---------------------
 		locations.GET("/new", func(c *gin.Context) {
 			c.HTML(http.StatusOK, "tmp_valid_locations.html", c.Keys)
 		})
 
-		// ------------- обработка формы добавления новой площадки ----------
 		locations.POST("/insert", app.AddLocation)
 
 		// td: как защититься от запросов не с этого сайта?
-		// ----------- обработка запроса от формы удаления площадки ------------
 		locations.Any("/delete/:ID", app.DeleteLocation)
 	}
 
@@ -93,15 +83,14 @@ func main() {
 	{
 		users.Use(app.GotoLoginIfNotLogged)
 		users.Use(app.GotoAccessDeniedIfNotAdmin)
-		// -------------- список ------------------
+
 		users.GET("/", app.ShowUsers)
 	}
 
 	// ======================= организаторы на площадках ===================
-	// ------------- список --------------------------------------------
+
 	router.GET("/locorgs", app.ShowLocorgs)
 
-	// ------------- добавление организатора на площадку ----------------
 	router.Any("/add_locorg", app.AddLocOrg)
 
 	// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> запуск! <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
